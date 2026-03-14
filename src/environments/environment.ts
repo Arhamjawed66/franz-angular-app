717eed239c922ba2730f4e6bc4b64335da0a9204
jawed shiekh <abduljawed6663@gmail.com>
2026-03-14 10:53:45 +0500

    iam working start

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-	apiUrl: 'http://localhost:8080/sb-inventory-service/api/v1'
-}
+    apiUrl: '/sb-inventory-service/api/v1' // <--- Sirf itna likhein
+};
